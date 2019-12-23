78c36eb02b55fe93b4e55443b99d77b3d797e3ac
Henry Liu <henry.liu@team.telstra.com>
2019-12-23 21:25:03 +1100

    adding some comments

--- run.sh
+++ run.sh
@@ -10,20 +10,29 @@ echo "5) For non-root account, privilege escalation method should be configured"
 read -p "Press enter to continue"
 clear
 
-apk add git && \
-git clone https://github.com/kubernetes-sigs/kubespray.git && pip3 install -r $PWD/kubespray/requirements.txt && cp -rfp kubespray/inventory/sample kubespray/inventory/mycluster
+# Declare env variables
 declare -a ALLIPS=$ALLIPS && \
 tLen=${#ALLIPS[@]}
+
+# Clone kubespray repository
+apk add git && \
+git clone https://github.com/kubernetes-sigs/kubespray.git && pip3 install -r $PWD/kubespray/requirements.txt && cp -rfp kubespray/inventory/sample kubespray/inventory/mycluster
+
+# Generate ssh key for this container and copy public key across target hosts
 ssh-keygen
 for (( i=0; i<${tLen}; i++ ));
 do
   ssh-copy-id ${AUSER}@${ALLIPS[$i]}
 done
+
+# Generate Ansible hosts file for this project
 echo "[servers]" > /etc/ansible/hosts && \
 for (( i=0; i<${tLen}; i++ ));
 do
   echo "node${i} ansible_host=${ALLIPS[$i]} ansible_user=${AUSER}" >> /etc/ansible/hosts
 done
+
+# Ansible Playbook start
 IPS=("${ALLIPS[@]}")
 CONFIG_FILE=$PWD/kubespray/inventory/mycluster/hosts.yaml python3 $PWD/kubespray/contrib/inventory_builder/inventory.py ${IPS[@]}
 /usr/bin/ansible-playbook --flush-cache -i $PWD/kubespray/inventory/mycluster/hosts.yaml  --become --become-user=root --ask-become-pass -e ansible_user=${AUSER} $PWD/kubespray/cluster.yml
