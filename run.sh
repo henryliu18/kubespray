8378bff8c0daef79443b9dc66fe2505ab853c7f0
Henry Liu <henry.hhl@gmail.com>
2019-12-25 21:35:44 +1100

    KEYFILE added

--- run.sh
+++ run.sh
@@ -20,32 +20,33 @@ function add_known_hosts () {
 # Declare env variables
 declare -a ALLIPS=$ALLIPS && \
 tLen=${#ALLIPS[@]}
-PPATH=/key
-PKEY=pkey.ppk
+PKEYPATH=/key
+PKEY=${KEYFILE}
 
-if [ -f "$PPATH/$PKEY" ]; then
-  echo "$PPATH/$PKEY will be used for SSH connection"
+if [ -f "$PKEYPATH/$PKEY" ]; then
+  echo "$PKEYPATH/$PKEY will be used for SSH connection"
 else
   echo "No private key found!  Password will be used for SSH connection"
 fi
 
 # Information
-echo "The host running docker MUST on the same network of $ALLIPS"
-echo "K8s hosts are $ALLIPS"
+clear
+echo "The host running docker MUST on the same network of ${ALLIPS[@]}"
+echo "K8s hosts are ${ALLIPS[@]}"
 echo "prerequisites are"
-echo "1) $ALLIPS must have access to the Internet"
-echo "2) $ALLIPS allow IPv4 forwarding"
-echo "3) Your ssh key must be copied to $ALLIPS"
-echo "3.1) If you are using private key for ssh connection, key folder needs to be mapped to /key, e.g. -v /key-folder:/key"
-echo "4) In order to avoid any issue during deployment you should disable your firewall on $ALLIPS"
+echo "1) ${ALLIPS[@]} must have access to the Internet"
+echo "2) ${ALLIPS[@]} allow IPv4 forwarding"
+echo "3) Your ssh key must be copied to ${ALLIPS[@]} which will be done in this program"
+echo "3.1) Or if you are using private key for ssh connection, key folder MUST be mapped to /key, e.g. -v /key-folder:/key"
+echo "4) In order to avoid any issue during deployment you should disable your firewall on ${ALLIPS[@]}"
 echo "5) For non-root account, privilege escalation method should be configured"
 read -p "Press enter to continue"
 clear
 
-# If $PPATH/$PKEY is NOT found then generate ssh key for this container and copy public key across target hosts
-# If $PPATH/$PKEY is found then establish a ssh connection using existing key which will add remote host to ~/.ssh/known_hosts in the container
-if [ -f "$PPATH/$PKEY" ]; then
-  add_known_hosts "$PPATH/$PKEY"
+# If $PKEYPATH/$PKEY is NOT found then generate ssh key for this container and copy public key across target hosts
+# If $PKEYPATH/$PKEY is found then establish a ssh connection using existing key which will add remote host to ~/.ssh/known_hosts in the container
+if [ -f "$PKEYPATH/$PKEY" ]; then
+  add_known_hosts "$PKEYPATH/$PKEY"
 else
   genkey
 fi
@@ -64,7 +65,7 @@ done
 IPS=("${ALLIPS[@]}")
 CONFIG_FILE=$PWD/kubespray/inventory/mycluster/hosts.yaml python3 $PWD/kubespray/contrib/inventory_builder/inventory.py ${IPS[@]}
 
-if [ -f "$PPATH/$PKEY" ]; then
+if [ -f "$PKEYPATH/$PKEY" ]; then
   /usr/bin/ansible-playbook --flush-cache -i $PWD/kubespray/inventory/mycluster/hosts.yaml  --become --become-user=root --private-key="/tmp/$PKEY" -e ansible_user=${AUSER} $PWD/kubespray/cluster.yml
 else
   /usr/bin/ansible-playbook --flush-cache -i $PWD/kubespray/inventory/mycluster/hosts.yaml  --become --become-user=root --ask-become-pass -e ansible_user=${AUSER} $PWD/kubespray/cluster.yml
