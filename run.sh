14b8bb97e361c748c79c5075b7aeae860dffe8be
Henry Liu <henry.hhl@gmail.com>
2019-12-25 13:49:39 +1100

    private key support

--- run.sh
+++ run.sh
@@ -17,33 +17,41 @@ function addknown_hosts () {
   done
 }
 
+# Declare env variables
+declare -a ALLIPS=$ALLIPS && \
+tLen=${#ALLIPS[@]}
+PPATH=/key
+PKEY=pkey.ppk
+
+if [ -f "$PPATH/$PKEY" ]; then
+  echo "$PPATH/$PKEY will be used for SSH connection"
+else
+  echo "No private key found!  Password will be used for SSH connection"
+fi
+
+# Information
 echo "The host running docker MUST on the same network of $ALLIPS"
 echo "K8s hosts are $ALLIPS"
 echo "prerequisites are"
 echo "1) $ALLIPS must have access to the Internet"
 echo "2) $ALLIPS allow IPv4 forwarding"
 echo "3) Your ssh key must be copied to $ALLIPS"
+echo "3.1) If you are using private key for ssh connection, key folder needs to be mapped to /key, e.g. -v /key-folder:/key"
 echo "4) In order to avoid any issue during deployment you should disable your firewall on $ALLIPS"
 echo "5) For non-root account, privilege escalation method should be configured"
 read -p "Press enter to continue"
 clear
 
-# Declare env variables
-declare -a ALLIPS=$ALLIPS && \
-tLen=${#ALLIPS[@]}
-
-# Clone kubespray repository
-apk add git && git clone https://github.com/kubernetes-sigs/kubespray.git && pip3 install -r $PWD/kubespray/requirements.txt && cp -rfp kubespray/inventory/sample kubespray/inventory/mycluster
-
 # If $PPATH/$PKEY is NOT found then generate ssh key for this container and copy public key across target hosts
 # If $PPATH/$PKEY is found then establish a ssh connection using existing key which will add remote host to ~/.ssh/known_hosts in the container
-PPATH=/key
-PKEY=pkey.ppk
 if [ -f "$PPATH/$PKEY" ]; then
   addknown_hosts "$PPATH/$PKEY"
 else
   genkey
 fi
+
+# Clone kubespray repository
+apk add git && git clone https://github.com/kubernetes-sigs/kubespray.git && pip3 install -r $PWD/kubespray/requirements.txt && cp -rfp kubespray/inventory/sample kubespray/inventory/mycluster
 
 # Generate Ansible hosts file for this project
 echo "[servers]" > /etc/ansible/hosts && \
